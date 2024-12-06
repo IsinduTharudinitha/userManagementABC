6dace78afdeafd1b17fca80a176f0deb6aea1edb
K G I Tharudinitha <121121883+IsinduTharudinitha@users.noreply.github.com>
2024-12-06 11:02:29 +0530

    Add files via upload

---
+++ UserManagementForABC/UserManagementForABC/src/main/java/com/Spring/UserManagementForABC/UserManagementForAbcApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.UserManagementForABC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementForAbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementForAbcApplication.class, args);
+	}
+
+}
---
+++ UserManagementForABC/UserManagementForABC/src/test/java/com/Spring/UserManagementForABC/UserManagementForAbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.UserManagementForABC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementForAbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
